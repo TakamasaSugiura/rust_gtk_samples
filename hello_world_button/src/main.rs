e61843c3ae0716131412fd8d3065ae92c3dac442
Takamasa Sugiura <takamasas2008@gmail.com>
2023-08-12 09:11:51 +0900

    add button application

---
+++ hello_world_button/src/main.rs
@@ -0,0 +1,54 @@
+// Copyright (C) 2023  Takamasa Sugiura
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+use glib::clone;
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::{Builder, Window, Button, Dialog};
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("my.app.hello_world"),
+        Default::default(),
+    );
+    application.connect_activate(build_ui);
+    application.run();
+}
+
+fn build_ui(application: &gtk::Application) {
+    let builder = Builder::from_file("resources/HelloWorldButton.glade");
+    let window = builder.object::<Window>("HelloWorldWindow").expect("Couldn't get HelloWorldWindow");
+    window.set_application(Some(application));
+    // set default size
+    window.set_default_size(300, 300);
+    // set min size
+    window.set_size_request(300, 300);
+    // set title
+    window.set_title("Hello world application");
+
+    let open_button = builder.object::<Button>("OpenButton").expect("Couldn't get OpenButton");
+    let dialog = builder.object::<Dialog>("HelloWorldDialog").expect("Couldn't get HelloWorldDialog");
+    let ok_button = builder.object::<Button>("OkButton").expect("Couldn't get OkButton");
+    dialog.connect_delete_event(|dialog,_| {
+       	dialog.hide();
+        Inhibit(true)
+    });
+    ok_button.connect_clicked(clone!(@weak dialog => move |_| dialog.close()));
+
+    open_button.connect_clicked(clone!(@weak dialog => move |_| dialog.show_all()));
+
+    // show main window
+    window.show_all();
+}
